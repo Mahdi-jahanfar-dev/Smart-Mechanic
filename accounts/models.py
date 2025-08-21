3864b85b651f51f8f6bb009f3aa60fb05a6fc56c
Mahdi-jahanfar-dev <mahdi.jahanfar01@gmail.com>
2025-08-22 01:47:47 +0330

    feat: added user model

---
+++ accounts/models.py
@@ -0,0 +1,26 @@
+from core.database_config import session, Base
+from sqlalchemy import Column, String, Integer
+from passlib.context import CryptContext
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(password: str) -> str:
+    pwd_context.hash(password)
+
+class User(Base):
+    
+    __tablename__ = "users"
+    
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    username = Column(String(length=100))
+    first_name = Column(String(length=100))
+    last_name = Column(String(length=100))
+    password = Column(String(length=200))
+    
+    def validate(self, entry_password):
+        pwd_context.verify(entry_password, self.password)
+        
+    def __repr__(self):
+        return self.username
+    
--- core/main.py
+++ core/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from .config import settings
+from accounts.models import User
+from .database_config import Base, engine
 
 
 app = FastAPI()
 
+
+Base.metadata.create_all(engine)
 
 @app.get("/")
 def test():
