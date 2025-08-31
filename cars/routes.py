34e541000c9a2682192d87f3f5200c316b3bd4c0
Mahdi-jahanfar-dev <mahdi.jahanfar01@gmail.com>
2025-08-31 12:38:38 +0330

    feat: added accept repair route

--- cars/routes.py
+++ cars/routes.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends, status
 from accounts.authentications import get_authenticated_user
 from core.database_config import get_db
-from .schema import CarRegisterSchema
+from .schema import CarRegisterSchema, AcceptCarRepairSchema
 from sqlalchemy.orm import Session
 from accounts.models import User
 from fastapi.exceptions import HTTPException
 from .models import Car
+from shops.models import MechanicReservation
 
 
 router = APIRouter(prefix="/cars", tags=["cars"])
@@ -45,3 +46,34 @@ async def cars_list_route(
     return cars
 
 
+@router.post("/accept-repair/{reservation_id}")
+async def accept_repair_route(reservation_id: int, data: AcceptCarRepairSchema, db: Session = Depends(get_db), user_id: int = Depends(get_authenticated_user)):
+    
+    reservation = db.query(MechanicReservation).filter_by(id = reservation_id).first()
+    
+    if not reservation.user_id == user_id:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="only reservation owner can user this route",
+        )
+    
+    if reservation.status == "received":
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="this reservation is closed",
+        )
+    
+    if data.repaired == True:
+        reservation.status = "received"
+        reservation.rating = data.rating
+        
+        db.commit()
+        db.refresh(reservation)
+        
+        return {"message": "your confirmation and rating registred"}
+    
+    reservation.status = "returned"
+    db.commit()
+    db.refresh(reservation)
+    
+    return {"message": "your reservation was returned"}
