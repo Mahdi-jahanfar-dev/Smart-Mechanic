a3d68a4866577ab9b7897317d3291db113c95810
mahdi <mahdi.jahanfar01@gmail.com>
2025-11-18 11:40:08 +0330

    feat: added basic problem db models

--- cars/models.py
+++ cars/models.py
@@ -23,6 +23,33 @@ class Car(Base):
         return f"{self.owner.username}-{self.brand}-{self.model}"
 
 
+# basic problem model (for problems like low engine oil, weak battery, etc.)
+class BasicProblemModel(Base):
+    __tablename__ = "basic_problems"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    description = Column(String)
+    low_engine_oil = Column(Boolean, default=False)
+    weak_battery = Column(Boolean, default=False)
+    flat_tire = Column(Boolean, default=False)
+    low_brake_oil = Column(Boolean, default=False)
+
+    main_problem_id = Column(
+        Integer,
+        ForeignKey(
+            "main_problems.id",
+        ),
+    )
+
+    main_prob = relationship(
+        "MainProblemModel",
+        backref=backref("basic_problems", cascade="all, delete-orphan"),
+    )
+
+    def __repr__(self):
+        return f"{self.car.brand}-{self.car.model}-{self.description}"
+
+
+# main problem model (this model will have relationship with all other problem models)
 class MainProblemModel(Base):
     __tablename__ = "main_problems"
     id = Column(Integer, primary_key=True, autoincrement=True)
