bb15387b05ce01e077eadcf445b173773487585b
mahdi <mahdi.jahanfar01@gmail.com>
2025-11-18 11:34:23 +0330

    feat: added main problem db model

--- cars/models.py
+++ cars/models.py
@@ -1,5 +1,5 @@
 from core.database_config import Base
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
 from sqlalchemy.orm import relationship, backref
 
 
@@ -21,3 +21,22 @@ class Car(Base):
 
     def __repr__(self):
         return f"{self.owner.username}-{self.brand}-{self.model}"
+
+
+class MainProblemModel(Base):
+    __tablename__ = "main_problems"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    description = Column(String)
+    car_id = Column(
+        Integer,
+        ForeignKey(
+            "cars.id",
+        ),
+    )
+
+    car = relationship(
+        "Car", backref=backref("main_problems", cascade="all, delete-orphan")
+    )
+
+    def __repr__(self):
+        return f"{self.car.brand}-{self.car.model}-{self.description}"
